1f39c1fd0f424a664ee3e0def72012b6f5a3e360
adalid.bori <adalid@Adalids-MacBook-Air.local>
2023-05-11 19:49:44 -0400

    before realease

---
+++ jqueryBtn.js
@@ -0,0 +1,5 @@
+var miBoton = document.getElementById("freePlanButton");
+miBoton.addEventListener("click", function() {
+    // Este código se ejecutará cuando se haga clic en el botón
+    console.log("Se hizo clic en el botón");
+  });
